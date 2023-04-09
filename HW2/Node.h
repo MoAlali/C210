8aba3d1353edb5d048fff6079e9a5dd3026e5afe
MoAlali <84029535+MoAlali@users.noreply.github.com>
2023-04-09 14:42:44 +0300

    Create Node.h

---
+++ HW2/Node.h
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+private:
+    /* data */
+public:
+    Node(/* args */);
+    ~Node();
+};
+
+Node::Node(/* args */)
+{
+}
+
+Node::~Node()
+{
+}
